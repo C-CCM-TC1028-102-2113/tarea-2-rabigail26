f4fab3a4442fa9d56ad4c098cacf6a03c445244b
rabigail26 <89092963+rabigail26@users.noreply.github.com>
2021-08-24 14:15:00 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -2,7 +2,22 @@
 def main():
     #Escribe tu código debajo de esta línea
     pass
-
+def mine():
+    
+    if edad <18:
+      print ('no cuentas con la edad')  
+    elif edad>=18:
+   
+      print('¿cuentas con identificacion?: (s/n)')
+      opcion=str(input())
+    if opcion =='s':
+      print ('solicitud aceptada')
+    elif opcion=='n':
+     print('no cuentas con los requisitos necesarios')
+    
+print ('ingresa tu edad')
+edad=int(input())
+mine ()
 
 if __name__ == '__main__':
     main()
