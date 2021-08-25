cf51278ea5c9a2e17778e21012ce33891cfe2af6
rabigail26 <89092963+rabigail26@users.noreply.github.com>
2021-08-25 09:52:02 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,7 +1,25 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+def main():
+    if x>y:
+        if x>z:
+            print("el número mayor es:", x)
+    if y>x:
+        if y>z:
+            print("el número mayor es:", y)
+    if z>x:
+        if z>y:
+            print("el número mayor es:", z)
+    
 
+print("Ingresa el primer número:")
+x=int(input())
+print("Ingresa el segundo número:")
+y=int(input())
+print("Ingresa el tercero número:")
+z=int(input())
+main()
 
 if __name__=='__main__':
     main()
